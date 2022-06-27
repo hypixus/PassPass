fe05abb9cbce591046e81de2dbff25ed518eb1f2
Piotr Rusin <rusinpiotr@outlook.com>
2022-06-27 02:24:42 +0200

    Added work-in-progress WPF project.

---
+++ PassPassClient/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PassPassClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            for (var i = 0; i < 50; i++)
+            {
+                CollectionListBox.Items.Add(new ListBoxItem()
+                {
+                    Content = "pepega"
+                });
+                EntriesListBox.Items.Add(new ListBoxItem()
+                {
+                    Content = "omegalul"
+                });
+            }
+        }
+        
+        private void CollectionListBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+        
+        private void EntriesListBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+    }
+}
